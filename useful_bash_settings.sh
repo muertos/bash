9b17badef63164bfe3b21d1647b195b90c9f8d76
Nick West <nickwe@inmotionhosting.com>
2021-10-13 10:21:08 -0400

    push

---
+++ useful_bash_settings.sh
@@ -0,0 +1,7 @@
+# Useful bash settings
+
+# Set the `errexit` option to make sure that
+# if one command fails, all the script execution
+# will also fail (see `man bash` for more
+# information on the options that you can set).
+set -o errexit
