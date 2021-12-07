6df693a6d189f67d3cfd68d99e0a663fb0bc8415
Nick West <nickwe@inmotionhosting.com>
2021-12-07 16:07:30 -0500

    push

--- sort-docker-volumes.sh
+++ sort-docker-volumes.sh
@@ -1,7 +1,7 @@
 # Obtains list of all Docker volumes and sorts by size, descending
 function sort-docker-volumes() {
   while read volume; do
-    docker volume inspect $volume | jq -r .[].Mountpoint
+    docker volume inspect $volume | jq -r ".[].Mountpoint"
   done <<< \
     $(docker volume ls | awk '{print $2}' | grep -v VOLUME) | \
     xargs du -sh | sort -rh
